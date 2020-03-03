41563b41970d0e3e5942d7243cbd7a2803206f36
Paula Marsal <paula.mz_376@live.com>
2020-03-03 20:24:31 +0100

    Api Movie funcionando en html

--- scripts/movieApiService.js
+++ scripts/movieApiService.js
@@ -1,45 +1,36 @@
 'use strict';
 
+let apiMovieURL = "https://api.themoviedb.org/3/discover/movie?api_key=35dace4745a4ee8ae13d0cf2fd62e8db&language=es-ES&include_adult=false&include_video=false&page=1&release_date.lte=2018&vote_average.gte=7";
+
+let movieArray,movie,year,section,article;
+let random = Math.round(Math.floor((Math.random() * 21)));
+
+
 const connectToApi = () => {
-    const section = document.querySelector('.movie');
-
-   // for (let i=0; i < 5; i++) {
-        fetch(`http://www.omdbapi.com/?apikey=5d86a00&s={{$randomWord}}&type=movie`)
-        .then(function (response) {
-            console.log(response);
-            return response.json();
-        })
-        .then(function (movie) {
-            const article = document.createElement('article');
-            article.innerHTML = `
-                <div>
-                    <h1>¡La palomitera ha decidido!</h1>
-                </div>
-
-                <div>
-                    <img src="${movie.Poster}" alt="poster-${movie.Title}">
-                    <h2>${movie.Title}</h2>
-                    <p><span>${movie.Year}</span> <span>${movie.Runtime}</span> <span>${movie.Rated}</span></p>
-                    <p><span>${movie.Genre}</span> <span>${movie.Country}</span></p>
-                    <p>Director: <span>${movie.Director}</span></p>
-                    <p>Reparto: <span>${movie.Actors}</span></p>
-                    <h3>Sinopsis</h3>
-                    <p>${movie.Plot}</p>
-                </div>
-
-                <div>
-                    <p>Si no te gusta esta elección:</p>
-                    <button type="reset">Prueba otra vez</button>
-                </div>
-                `;
-            section.appendChild(article);
-            })
-            .catch(function (error) {
-                return error;
-            });
-    //}
-}
-
-window.onload = connectToApi();
+    fetch(apiMovieURL)
+    .then (response => response.json())
+    .then (data => movieArray = data.results)
+    .then (movieArrayData => movie = movieArray[random])
+    .then (getElement => document.getElementById("movie").innerHTML = `
+    <div>
+        <h1>¡La palomitera ha decidido!</h1>
+    </div>
+    <div>
+        <img src="http://image.tmdb.org/t/p/w185//${movie.poster_path}" alt="poster-${movie.title}">
+        <h3>${movie.title}</h3>
+        <p><span>Año: ${movie.release_date.split("-")[0]}</span> <span>Puntuación :${movie.vote_average}</span></p>
+        <h4>Sinopsis</h4>
+        <p>${movie.overview}</p>
+    </div>
+    <div>
+        <p>Si no te gusta esta elección:</p>
+        <button type="reset">Prueba otra vez</button>
+    </div>
+    `)
+    .catch(function (error) {
+        return error;
+    });
+};
 
 
+window.onload = connectToApi;
